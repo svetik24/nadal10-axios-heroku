fea02969222611acdc0d945cbcf3b5d7ff50bc57
ees-ja perekonnanimi <svetik24@hotmail.com>
2024-05-03 21:05:58 +0300

    add getmovie route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const axios = require("axios");
-
+const http = require("http");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.get("/", (req, res) =>{
     let url = "https://api.themoviedb.org/3/movie/787699?api_key=87a32ef55335ba35e416797010a0e286";
@@ -92,6 +94,52 @@ let currentYear = new Date().getFullYear();
         
         });
 
-app.listen(3000, () => { 
+//new route
+app.post('/getmovie', (req, res) => {
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9d1b`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
+
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
+});
+
+app.listen(process.env.PORT || 3000, () => { 
 console.log("Server is running on Port 3000");
 });
